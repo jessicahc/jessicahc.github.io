317aeeba094290a1eda48b120a6b8953a39195d2
jessicahc <119449064+jessicahc@users.noreply.github.com>
2023-04-25 10:25:30 -0400

    add camera files

---
+++ navigation.js
@@ -0,0 +1,13 @@
+let menuIcon = document.querySelector('svg.menu');
+let navigation = document.querySelector('nav');
+let header = document.querySelector('header');
+
+function toggleNav() {
+  // console.log('toggle');
+  navigation.classList.toggle('toggle');
+  // navigation.className = "toggle";
+  header.classList.toggle('toggle');
+  // header.className = "toggle";
+}
+
+menuIcon.addEventListener('click', toggleNav);
